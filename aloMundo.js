6fc9adb33dd61881a21052d52ea2180b5e016104
eduardo.saatkamp <eduardo.saatkamp@dataprev.gov.br>
2024-02-12 18:41:31 -0300

    HelloWorld compilado e testado com typescript

---
+++ aloMundo.js
@@ -0,0 +1,9 @@
+/* Note que este formato não funciona em JavaScript
+
+let texto:string = "Hello World";
+console.log(texto); 
+
+ Mas funciona em outro formato*/
+
+let texto = "Alô mundo";
+console.log(texto); 
---
+++ helloworld.ts
@@ -0,0 +1,10 @@
+//variável mensagem com tipo definido (string)
+
+let mensagem:string = "Hello World";
+
+//console.log recebendo como parâmetro a variável 'mensagem',
+//para exibir o texto associado a ela no console do navegador.
+
+/* Não esquecer de compilar o código: Para isso, vamos acessar a pasta onde nosso projeto foi criado através do prompt de comando e executar o comando tsc helloworld.ts  */
+
+console.log(mensagem);
